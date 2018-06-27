7299868a85110ee5a371a28466b39a3a2a18735d
Bill St. Clair <billstclair@gmail.com>
2018-06-27 05:28:02 -0400

    First commit.

---
+++ src/Recovered/Version.elm
@@ -0,0 +1,14 @@
+module Recovered.Version exposing (version)
+
+{-| This module tracks the package version. See README.md for details.
+
+@docs version
+
+-}
+
+
+{-| Updated to match the package version.
+-}
+version : String
+version =
+    "1.0.0"
